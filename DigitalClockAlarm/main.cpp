0770f3bee329345ec9ca8d5c2b9d3cddf7e744a6
masterchris101 <c.novelleruddy@bhcc.edu>
2025-01-02 20:04:07 -0500

    Initial Commit

---
+++ DigitalClockAlarm/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DigitalClockAlarm
+//
+//  Created by Cristian Novelle Ruddy on 1/2/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
